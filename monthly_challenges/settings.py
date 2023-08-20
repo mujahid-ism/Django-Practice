13c2efaed5d585cd94e6ac7974459c4e79a28e1e
mujahid-ism <mujahidul.islam@selise.ch>
2023-08-21 00:19:38 +0600

    static and use css

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Adding Global Static Directory
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
