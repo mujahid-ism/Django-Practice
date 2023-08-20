055dd6c838666a788f11aa7e5b5f16653ec99bb2
mujahid-ism <mujahidul.islam@selise.ch>
2023-08-20 16:01:20 +0600

    first commit

---
+++ challenges/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+# Create your views here.
+
+monthly_challenges = {
+    'january': 'January, That is the First month month of the year',
+    'february': 'February, That is the Second month month of the year',
+    'march': 'March, That is the Third month month of the year',
+    'april': None
+}
+
+
+def index(request):
+    # month_link = ''
+    month_list = list(monthly_challenges.keys())
+
+    # for month in month_list:
+    #     month_path = reverse('month_challenge', args=[month])
+    #     month_link += f"<li> <a href = '{month_path}'>{month}</a></li>"
+
+    # list_data = f'<ol> {month_link} </ol>'
+
+    # return HttpResponse(list_data)
+
+    return render(request, 'challenges/index.html', {'month_list': month_list})
+
+
+def monthly_challenge_by_number(request, month):
+    if month > 3:
+        return HttpResponseNotFound('Wrong interger number')
+    else:
+        month_list = list(monthly_challenges.keys())
+        redirect_month = month_list[month - 1]
+        redirect_url = reverse('month_challenge', args=[redirect_month])
+
+        return HttpResponseRedirect(redirect_url)
+
+
+def monthly_challenge(request, month):
+    try:
+        month_challenge = monthly_challenges[month]
+        # response_data = render_to_string('challenges/challenge.html')
+        # return HttpResponse(response_data)
+        return render(request, 'challenges/challenge.html', {'month': month, 'month_challenge': month_challenge})
+    except:
+        response_data = render_to_string('404.html')
+        return HttpResponseNotFound(response_data)
